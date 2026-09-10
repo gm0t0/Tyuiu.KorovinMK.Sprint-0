2c5bf9edc8bab46a10b73a1316abf7eed55bf88e
Пользователь <@DESKTOP-TV1F3FH>
2026-09-10 16:20:27 +0500

    Add project files.

---
+++ Tyuiu.KorovinMK.Sprint0.Task0.V0/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.KorovinMK.Sprint0.Task0.V0
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
